c45c08095203b4a378e12c15ee9e3496705d16e6
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-03-05 13:29:13 +0100

    Update index.php

--- login/public_html/index.php
+++ login/public_html/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./ressources/css/styles.css"/>
-    <title>Login con PHP y Sessions</title>
+    <title>AI MOVIE CLUB</title>
 </head>
 <body>
     <main>
@@ -13,16 +13,16 @@
         <form action="login.php" method="POST" id="login-form">
             <div class="input-form">
                 <label for="email">Email:</label>
-                <input type="email" name="email" id="email" placeholder="Escribe tu email"/>
+                <input type="email" name="email" id="email" placeholder="email"/>
             </div>
             <div class="input-form">
                 <label for="password">Password:</label>
-                <input type="password" name="password" id="password" placeholder="Introduce tu contraseña"/>
+                <input type="password" name="password" id="password" placeholder="Enter your password"/>
             </div>
 
-            <button type="submit" form="login-form" value="Submit">Ingresar</button>
+            <button type="submit" form="login-form" value="Submit">Login</button>
             <p>
-                ¿No tienes usuario? Entonces puedes crear una cuenta <a href="register.php" class="white">aquí</a>
+                Don't have a user? Then you can create an account <a href="register.php" class="white">Here</a>
             </p>
         </form>
     </main>
