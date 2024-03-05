702c7dba9f4cecbc8f0bcec7d72e729c230e6416
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-03-05 13:44:07 +0100

    Update header.php

--- login/public_html/includes/header.php
+++ login/public_html/includes/header.php
@@ -3,23 +3,23 @@
     session_start();
 
     if(!isset($_SESSION['valid'])){
-        header('Location: index.php');
+        header('Location: form.php');
     }
 
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./ressources/css/styles.css"/>
-    <title>Login con PHP y Sessions</title>
+    <title>AI MOVIE CLUB</title>
 </head>
 <body>
     <header>
         <img src="./ressources/img/garaje-logo.jpg" width="100px" height="100px">
         <div id="menu-user">
             <p>Hola <?php echo $_SESSION['email'];?> !</p>
-            <a href="logout.php" id="logout-link">Cerrar sesion</a>
+            <a href="logout.php" id="logout-link">Sign out/a>
         </div>
     </header>
